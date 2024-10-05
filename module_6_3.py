5e9a7b90cbdff24dd4fe91de812e205474736ae3
anatoliiprk <anatoliiprk@gmail.com>
2024-10-05 20:56:48 +0700

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,14 +1,13 @@
 print(f'------\nЗадача "Мифическое наследование"\n------')
 
 class Horse:
-    def __init__(self, x_distance = 0, y_distance = 0, sound = 'Frrr', dy = 0):
+    def __init__(self, x_distance = 0, y_distance = 0, sound = 'Frrr'):
         self.x_distance = x_distance
         self.sound = sound
         super().__init__(y_distance)
 
     def run(self, dx):
         self.x_distance += dx
-        return self.x_distance
 
 class Eagle:
     def __init__(self, y_distance = 0, sound = 'I train, eat, sleep, and repeat'):
@@ -17,13 +16,12 @@ class Eagle:
 
     def fly(self, dy):
         self.y_distance += dy
-        return self.y_distance
 
 class Pegasus(Horse, Eagle):
     def __init__(self, x_distance = 0, y_distance = 0, sound = '', dx = 0, dy = 0):
         super().__init__(x_distance, y_distance, sound)
-        super().run(dx)
-        super().fly(dy)
+        Horse.run(self, dx)
+        Eagle.fly(self, dy)
 
     def move(self, dx: int, dy: int):
         self.run(dx)
